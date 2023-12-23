fa8f6061933d06092ff189d02f8c10ac6a95500f
kzaman3055 <kzaman3055@gmail.com>
2023-12-23 20:09:58 +0600

    fix: donor route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,7 +31,7 @@ class HomeController extends Controller
                 return view('admin.index');
 
             case 2: // donor
-                return view('donor.index');
+                return view('frontend.index');
 
             default:
                 return view('Admin.admin_master');
