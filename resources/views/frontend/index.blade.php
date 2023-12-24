a4364f6f8efb265f8223b462044eb44555c9ef9f
kzaman3055 <kzaman3055@gmail.com>
2023-12-24 18:25:05 +0600

    fix: available unite of  blood

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -208,105 +208,70 @@
                         <div class="col-lg-3 offset-lg-0 col-md-5 offset-md-1 col-sm-6">
                             <div class="category-block">
                                 <div class="header">
-                                    <i class="fa fa-laptop icon-bg-1"></i>
-                                    <h4>Electronics</h4>
+                                    <i class="fa fa-plus-square icon-bg-1"></i>
+                                    <h4>A+</h4>
                                 </div>
-                                <ul class="category-list">
-                                    <li><a href="#">Laptops <span>93</span></a></li>
-                                    <li><a href="#">Iphone <span>233</span></a></li>
-                                    <li><a href="#">Microsoft <span>183</span></a></li>
-                                    <li><a href="#">Monitors <span>343</span></a></li>
-                                </ul>
+                            <h4 style="text-align: center">Alailable Unit: {{ \App\Models\admin\Inventory::where('blood_group', 'A+')->where('expire_date', '>', now())->where('remain_unit', '>=', 0)->sum('remain_unit') }}</h4>
                             </div>
                         </div> <!-- /Category List -->
                         <!-- Category list -->
                         <div class="col-lg-3 offset-lg-0 col-md-5 offset-md-1 col-sm-6">
                             <div class="category-block">
                                 <div class="header">
-                                    <i class="fa fa-apple icon-bg-2"></i>
-                                    <h4>Restaurants</h4>
+                                    <i class="fa fa-plus-square icon-bg-2"></i>
+                                    <h4>A-</h4>
                                 </div>
-                                <ul class="category-list">
-                                    <li><a href="#">Cafe <span>393</span></a></li>
-                                    <li><a href="#">Fast food <span>23</span></a></li>
-                                    <li><a href="#">Restaurants <span>13</span></a></li>
-                                    <li><a href="#">Food Track<span>43</span></a></li>
-                                </ul>
+                                <h4 style="text-align: center">Alailable Unit: {{ \App\Models\admin\Inventory::where('blood_group', 'A-')->where('expire_date', '>', now())->where('remain_unit', '>=', 0)->sum('remain_unit') }}</h4>
                             </div>
                         </div> <!-- /Category List -->
                         <!-- Category list -->
                         <div class="col-lg-3 offset-lg-0 col-md-5 offset-md-1 col-sm-6">
                             <div class="category-block">
                                 <div class="header">
-                                    <i class="fa fa-home icon-bg-3"></i>
-                                    <h4>Real Estate</h4>
+                                    <i class="fa fa-plus-square icon-bg-3"></i>
+                                    <h4>B+</h4>
                                 </div>
-                                <ul class="category-list">
-                                    <li><a href="#">Farms <span>93</span></a></li>
-                                    <li><a href="#">Gym <span>23</span></a></li>
-                                    <li><a href="#">Hospitals <span>83</span></a></li>
-                                    <li><a href="#">Parolurs <span>33</span></a></li>
-                                </ul>
+                                <h4 style="text-align: center">Alailable Unit: {{ \App\Models\admin\Inventory::where('blood_group', 'B+')->where('expire_date', '>', now())->where('remain_unit', '>=', 0)->sum('remain_unit') }}</h4>
                             </div>
                         </div> <!-- /Category List -->
                         <!-- Category list -->
                         <div class="col-lg-3 offset-lg-0 col-md-5 offset-md-1 col-sm-6">
                             <div class="category-block">
                                 <div class="header">
-                                    <i class="fa fa-shopping-basket icon-bg-4"></i>
-                                    <h4>Shoppings</h4>
+                                    <i class="fa fa-plus-square icon-bg-4"></i>
+                                    <h4>B-</h4>
                                 </div>
-                                <ul class="category-list">
-                                    <li><a href="#">Mens Wears <span>53</span></a></li>
-                                    <li><a href="#">Accessories <span>212</span></a></li>
-                                    <li><a href="#">Kids Wears <span>133</span></a></li>
-                                    <li><a href="#">It & Software <span>143</span></a></li>
-                                </ul>
+                                <h4 style="text-align: center">Alailable Unit: {{ \App\Models\admin\Inventory::where('blood_group', 'B-')->where('expire_date', '>', now())->where('remain_unit', '>=', 0)->sum('remain_unit') }}</h4>
                             </div>
                         </div> <!-- /Category List -->
                         <!-- Category list -->
                         <div class="col-lg-3 offset-lg-0 col-md-5 offset-md-1 col-sm-6">
                             <div class="category-block">
                                 <div class="header">
-                                    <i class="fa fa-briefcase icon-bg-5"></i>
-                                    <h4>Jobs</h4>
+                                    <i class="fa fa-plus-square icon-bg-5"></i>
+                                    <h4>AB+</h4>
                                 </div>
-                                <ul class="category-list">
-                                    <li><a href="#">It Jobs <span>93</span></a></li>
-                                    <li><a href="#">Cleaning & Washing <span>233</span></a></li>
-                                    <li><a href="#">Management <span>183</span></a></li>
-                                    <li><a href="#">Voluntary Works <span>343</span></a></li>
-                                </ul>
+                                <h4 style="text-align: center">Alailable Unit: {{ \App\Models\admin\Inventory::where('blood_group', 'AB+')->where('expire_date', '>', now())->where('remain_unit', '>=', 0)->sum('remain_unit') }}</h4>
                             </div>
                         </div> <!-- /Category List -->
                         <!-- Category list -->
                         <div class="col-lg-3 offset-lg-0 col-md-5 offset-md-1 col-sm-6">
                             <div class="category-block">
                                 <div class="header">
-                                    <i class="fa fa-car icon-bg-6"></i>
-                                    <h4>Vehicles</h4>
+                                    <i class="fa fa-plus-square icon-bg-6"></i>
+                                    <h4>AB-</h4>
                                 </div>
-                                <ul class="category-list">
-                                    <li><a href="#">Bus <span>193</span></a></li>
-                                    <li><a href="#">Cars <span>23</span></a></li>
-                                    <li><a href="#">Motobike <span>33</span></a></li>
-                                    <li><a href="#">Rent a car <span>73</span></a></li>
-                                </ul>
+                                <h4 style="text-align: center">Alailable Unit: {{ \App\Models\admin\Inventory::where('blood_group', 'AB-')->where('expire_date', '>', now())->where('remain_unit', '>=', 0)->sum('remain_unit') }}</h4>
                             </div>
                         </div> <!-- /Category List -->
                         <!-- Category list -->
                         <div class="col-lg-3 offset-lg-0 col-md-5 offset-md-1 col-sm-6">
                             <div class="category-block">
                                 <div class="header">
-                                    <i class="fa fa-paw icon-bg-7"></i>
-                                    <h4>Pets</h4>
+                                    <i class="fa fa-plus-square icon-bg-7"></i>
+                                    <h4>O+</h4>
                                 </div>
-                                <ul class="category-list">
-                                    <li><a href="#">Cats <span>65</span></a></li>
-                                    <li><a href="#">Dogs <span>23</span></a></li>
-                                    <li><a href="#">Birds <span>113</span></a></li>
-                                    <li><a href="#">Others <span>43</span></a></li>
-                                </ul>
+                                <h4 style="text-align: center">Alailable Unit: {{ \App\Models\admin\Inventory::where('blood_group', 'O+')->where('expire_date', '>', now())->where('remain_unit', '>=', 0)->sum('remain_unit') }}</h4>
                             </div>
                         </div> <!-- /Category List -->
                         <!-- Category list -->
@@ -314,15 +279,10 @@
                             <div class="category-block">
 
                                 <div class="header">
-                                    <i class="fa fa-laptop icon-bg-8"></i>
-                                    <h4>Services</h4>
+                                    <i class="fa fa-plus-square icon-bg-8"></i>
+                                    <h4>O-</h4>
                                 </div>
-                                <ul class="category-list">
-                                    <li><a href="#">Cleaning <span>93</span></a></li>
-                                    <li><a href="#">Car Washing <span>233</span></a></li>
-                                    <li><a href="#">Clothing <span>183</span></a></li>
-                                    <li><a href="#">Business <span>343</span></a></li>
-                                </ul>
+                                <h4 style="text-align: center">Alailable Unit: {{ \App\Models\admin\Inventory::where('blood_group', 'O-')->where('expire_date', '>', now())->where('remain_unit', '>=', 0)->sum('remain_unit') }}</h4>
                             </div>
                         </div> <!-- /Category List -->
 
