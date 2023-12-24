3b820ea273f3c2711af90b3ac0b63834837ed8e0
zidankhan86 <zidankhan718@gmail.com>
2023-12-24 16:59:29 +0600

    fix:addrss nullable issue

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,10 @@ return new class extends Migration
             $table->string('name');
             $table->boolean('user_type')
             ->nullable()
-            ->comment('1: admin, 2: donor, 3: patient, ');
+            ->comment('1: admin, 2: donor, 3: x, 4: y, 5: x,');
+            $table->string('address')->nullable();
+            $table->string('blood_group')->nullable();
+            $table->string('phone')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
